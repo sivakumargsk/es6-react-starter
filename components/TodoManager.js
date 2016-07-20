48a8e4a6f0b55cfde5d5a925f00a97a447cbd1d5
sivakumargsk <sivakumar.gsk@technoidentity.com>
2016-07-20 11:55:49 +0530

    addOnClick function is added for changing the state with new latest task

--- components/TodoManager.js
+++ components/TodoManager.js
@@ -5,19 +5,31 @@ import TodoFotter from './TodoFotter.js';
 
 // yasnippets
 
-const todos = [
-    {id: 1, text: 'Buy milk', active: true},
-    {id: 2, text: 'Do laundryyyyy', active: false},
-    {id: 3, text: 'Read a book', active: true}
-];
+var i = 4;
 
 export default React.createClass({
+  getInitialState: function() {
+    return {
+      data: [{id: 1, text: 'Buy milk', active: true},
+             {id: 2, text: 'Do laundryyyyy', active: false},
+             {id: 3, text: 'Read a book', active: true}],
+      nextTaskId: 4 };
+  },
+  addOnClick : function (){
+    var newTask = this.state.data.concat(
+                   [{ id: this.state.nextTaskId,
+                      text: document.getElementById('taskInput').value,
+                      active: false }]);
+    this.setState({data: newTask,
+                   nextTaskId: this.state.nextTaskId + 1 });
+    document.getElementById('taskInput').value = '';
+  },
   render: function() {
     return (
       <div>
-      <NewTodo />
-      <TodoList todos={todos} />
-      <TodoFotter todos={todos} />
+      <NewTodo addOnClick={this.addOnClick}/>
+      <TodoList todos={this.state.data} />
+      <TodoFotter todos={this.state.data} />
       </div>
     );
   }
