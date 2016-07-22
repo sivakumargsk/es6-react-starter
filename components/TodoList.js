3f9f9d8d81bc3aaafddbd0ed09d526f36d49641b
sivakumargsk <sivakumar.gsk@technoidentity.com>
2016-07-22 13:30:55 +0530

    added getVisibility filter function

--- components/TodoList.js
+++ components/TodoList.js
@@ -4,15 +4,11 @@ const TodoRow = React.createClass({
   render: function () {
     return (
       <li>
-        <div>
-          <span
-            // onClick={this.props.toggleTodo}
-            style= {{textDecoration: this.props.status ? 'line-through' : 'none',
-                     cursor: 'pointer'}} >
-              {this.props.text}
-          </span>
-          <input type='button' value='Remove' />
-        </div>
+        <span
+          style={{color: this.props.status ? 'green' : 'red', cursor: 'pointer'}}
+          onClick={this.props.toggleTodo}>
+            {this.props.text} </span>
+        <input type='button' value='Remove' onClick={this.props.removeTodo} />
       </li>
     );
   }
@@ -21,16 +17,40 @@ const TodoRow = React.createClass({
 export default React.createClass({
   genTodoRow: function(todo) {
     return (
-      <TodoRow key={todo.id}
-               text={todo.text}
-               status={todo.active} />
+      <TodoRow
+        key={todo.id}
+        text={todo.text}
+        status={todo.active}
+        toggleTodo = {this.props.toggleTodo.bind(null, todo)}
+        removeTodo ={this.props.removeTodo.bind(null, todo)} />
     );
   },
+
+  getVisibleTodos: function (todos, filter){
+    switch (filter) {
+      case 'SHOW_ALL':
+            return todos;
+      case 'SHOW_COMPLETED':
+            return todos.filter(function (i){
+                return i.active; }
+        );
+      case 'SHOW_ACTIVE':
+            return todos.filter(function (i){
+                return !i.active; }
+       );
+       default:
+            return todos;
+    }
+  },
+  showTodoList: function(todos, filterType) {
+    return this.getVisibleTodos(todos, filterType).map(this.genTodoRow);
+  },
+
   render: function() {
     return (
       <div>
         <h1>Todo List</h1>
-          <ul> {this.props.todos.map(this.genTodoRow)} </ul>
+          <ul> {this.showTodoList(this.props.todos, this.props.visibilityType)} </ul>
       </div>
     );
   }
